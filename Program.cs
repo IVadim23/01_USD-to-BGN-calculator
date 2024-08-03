9394fa8e70b4567487f7070a7466ac3d5daec454
IVadim23 <inazrski@gmail.com>
2024-08-03 23:57:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _USD_to_BGN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double bgn = usd * 1.79549;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("The price in USD is : {0:f2} ",usd);
+            Console.WriteLine(" |     |       |      |     " );
+            Console.WriteLine(" V     V       V      V     " );
+            Console.WriteLine("The price in BGN is : {0:f2} ",bgn);
+
+        }
+    }
+}
